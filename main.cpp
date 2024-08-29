d515e2ac48d590af8b8f80e137461ac71417e0ac
priyanshu <idealpriyanshu@gmail.com>
2024-08-30 01:47:20 +0530

    Classes for Growth and Inflation with functions

--- main.cpp
+++ main.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 using namespace std;
+
+class GrowthPrice {
+private:
+    vector<double> growthPrice;
+public:
+    void storeGrowthPrice(double growth_price) {
+        growthPrice.push_back(growth_price);
+    }
+    double getGrowthPrice(int year) const {
+        return growthPrice[year - 2001];
+    }
+};
+class Inflation {
+private:
+    vector<double> inflationRate;
+public:
+    void storeInflationRate(double inflation_value) {
+        inflationRate.push_back(inflation_value);
+    }
+    double getInflationRate(int year) const {
+        return inflationRate[year - 2001];
+    }
+};
 
 int main() {
     double costprice = 5000000;
