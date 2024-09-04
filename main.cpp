c959ba2b4b928747524f3876573b2b3759a6710d
priyanshu <idealpriyanshu@gmail.com>
2024-09-05 02:24:17 +0530

    Comments and docstrings for better clarity

--- main.cpp
+++ main.cpp
@@ -51,7 +51,15 @@ private:
     double ltcgTax = 0;
 
 public:
+    /**
+     * Calculates the Long Term Capital Gains (LTCG) Tax applicable
+     * given the net profit.
+     *
+     * @param profit The net profit.
+     * @return The LTCG Tax applicable.
+     */
     double getLTCGTax(double profit) {
+        // Calculate the LTCG Tax applicable
         ltcgTax = profit * 20 / 100;
         return ltcgTax;
     }
@@ -65,43 +73,55 @@ private:
     double initialCost;
     int initialYear;
     double sellingPrice;
-    double profitBase;
+    double netProfit;
 
 public:
     LTCG(double initial_cost, int initial_year, int size): growthPrice(size), inflationobj(size) {
         initialCost = initial_cost;
         initialYear = initial_year;
         sellingPrice = initial_cost;
-        profitBase = 0;
+        netProfit = 0;
     }
 
+    /**
+     * Calculates the selling price and LTCG tax applicable given the selling year.
+     *
+     * @param sell_year The year of selling.
+     */
     void calculate(int sell_year) {
         double ltcg_tax = 0;
         for (int i = initialYear; i < sell_year; i++) {
             double gp = growthPrice.getGrowthPrice(i);
             double inf = inflationobj.getInflationRate(i);
+            // Calculate the selling price
             sellingPrice *= (1 + (gp - inf) / 100);
         }
-        profitBase = sellingPrice - initialCost;
+        netProfit = sellingPrice - initialCost;
+        // Precision as 2 decimal places.
         cout << setprecision(2);
-        if (profitBase > 0) {
-            cout << "Net profit: " << profitBase << endl;
-            ltcg_tax = profitBase * 20 / 100;
-        }
-        else
-            cout << "No net profit (", profitBase, ") , therefore no LTCG tax applicable." << endl;
+        if (netProfit > 0) {
+            cout << "Net profit: " << netProfit << endl;
+            ltcg_tax = netProfit * 20 / 100;
+        } else
+            cout << "No net profit (" << fixed << netProfit << "), therefore no LTCG tax applicable." << endl;
         cout << fixed << "Selling Price: " << sellingPrice << endl;
         cout << fixed << "LTCG Tax applicable: " << ltcg_tax << endl;
     }
 
+    /**
+     * Reads the price-inflation.csv file and stores the data in the member variables.
+     */
     void parseInput() {
         string text;
         ifstream input_file("price-inflation.csv");
         if (!input_file.is_open()) throw runtime_error("Error opening file");
+        // Ignore the header line
         getline(input_file, text);
+        // Process the data
         while (getline(input_file, text)) {
             double growthprice = stof(text.substr(text.find_first_of(',') + 1, text.find_last_of(',')));
             double inflationrate = stof(text.substr(text.find_last_of(',') + 1));
+            // Store the growth price and inflation rate
             growthPrice.storeGrowthPrice(growthprice);
             inflationobj.storeInflationRate(inflationrate);
         }
@@ -109,6 +129,10 @@ public:
     }
 };
 
+/**
+ * Calculates the selling price and Long Term Capital Gains (LTCG) Tax applicable
+ * given the selling year.
+ */
 int main() {
     double costprice;
     int buyyear;
@@ -119,10 +143,13 @@ int main() {
     int sellyear;
     cout << "Enter the year of selling (between " << buyyear << " and 2029): ";
     cin >> sellyear;
+    // Check the year of selling is in the valid range
     if (sellyear < buyyear || sellyear >= 2030) {
         throw out_of_range("Year must be between " + to_string(buyyear) + " and 2029.");
     }
     LTCG ltcg(costprice, buyyear, 29);
+    // Read the price-inflation.csv file and store the data in the member variables
     ltcg.parseInput();
+    // Calculate the selling price and LTCG tax applicable
     ltcg.calculate(sellyear);
 }
